c5c81360e8e3357e679aa70ce5e41d9279bb0ebd
Eleaus-Hossain-Evan <evanhossain62@gmail.com>
2022-06-07 10:07:14 +0600

    fix all

--- lib/utils/fcm_setup.dart
+++ lib/utils/fcm_setup.dart
@@ -1,74 +1,74 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import '../utils/constants.dart';
-import 'local_notification.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter/material.dart';
+// import '../utils/constants.dart';
+// import 'local_notification.dart';
 
-ValueNotifier<String> deviceToken = ValueNotifier("");
+// ValueNotifier<String> deviceToken = ValueNotifier("");
 
-class FCMSetup with ChangeNotifier {
+// class FCMSetup with ChangeNotifier {
 
-  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  LocalNotification _localNotification;
+//   FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+//   LocalNotification _localNotification;
 
-  void conFigureFirebase(BuildContext context) {
+//   void conFigureFirebase(BuildContext context) {
 
-    _localNotification = LocalNotification();
-    _localNotification.initializePlugin(context);
+//     _localNotification = LocalNotification();
+//     _localNotification.initializePlugin(context);
 
-    _firebaseMessaging.requestNotificationPermissions(
-        IosNotificationSettings(sound: true, badge: true, alert: true)
-    );
+//     _firebaseMessaging.requestNotificationPermissions(
+//         IosNotificationSettings(sound: true, badge: true, alert: true)
+//     );
 
-    _firebaseMessaging.getToken().then((token) {
+//     _firebaseMessaging.getToken().then((token) {
 
-      print(token);
+//       print(token);
 
-      deviceToken.value = token;
-      _firebaseMessaging.subscribeToTopic(Constants.ALL_CUSTOMER);
-    });
+//       deviceToken.value = token;
+//       _firebaseMessaging.subscribeToTopic(Constants.ALL_CUSTOMER);
+//     });
 
-    try {
-      _firebaseMessaging.configure(
-        onMessage: notificationOnMessage,
-        onLaunch: notificationOnLaunch,
-        onResume: notificationOnResume,
-      );
-    } catch (error) {
-      print(error);
-    }
-  }
+//     try {
+//       _firebaseMessaging.configure(
+//         onMessage: notificationOnMessage,
+//         onLaunch: notificationOnLaunch,
+//         onResume: notificationOnResume,
+//       );
+//     } catch (error) {
+//       print(error);
+//     }
+//   }
 
-  void subscribeToTopic(String topic) {
+//   void subscribeToTopic(String topic) {
 
-    _firebaseMessaging.subscribeToTopic(topic);
-  }
+//     _firebaseMessaging.subscribeToTopic(topic);
+//   }
 
-  Future notificationOnResume(Map<String, dynamic> message) async {
+//   Future notificationOnResume(Map<String, dynamic> message) async {
 
-    print(message);
-  }
+//     print(message);
+//   }
 
-  Future notificationOnLaunch(Map<String, dynamic> message) async {
+//   Future notificationOnLaunch(Map<String, dynamic> message) async {
 
-    print(message);
-  }
+//     print(message);
+//   }
 
-  Future notificationOnMessage(Map<String, dynamic> message) async {
+//   Future notificationOnMessage(Map<String, dynamic> message) async {
 
-    print(message);
+//     print(message);
 
-    String title = message['notification']['title'];
-    String body = message['notification']['body'];
+//     String title = message['notification']['title'];
+//     String body = message['notification']['body'];
 
-    if(message['data']['image'] != null && message['data']['image'].toString().isNotEmpty) {
+//     if(message['data']['image'] != null && message['data']['image'].toString().isNotEmpty) {
 
-      _localNotification.notificationWithImage(_localNotification.generalChannelID, _localNotification.generalChannelName,
-          _localNotification.generalChannelDescription, title, body, message['data']['image']);
-    }
-    else {
+//       _localNotification.notificationWithImage(_localNotification.generalChannelID, _localNotification.generalChannelName,
+//           _localNotification.generalChannelDescription, title, body, message['data']['image']);
+//     }
+//     else {
 
-      _localNotification.generalNotification(_localNotification.generalChannelID, _localNotification.generalChannelName,
-          _localNotification.generalChannelDescription, title, body);
-    }
-  }
-}
+//       _localNotification.generalNotification(_localNotification.generalChannelID, _localNotification.generalChannelName,
+//           _localNotification.generalChannelDescription, title, body);
+//     }
+//   }
+// }
