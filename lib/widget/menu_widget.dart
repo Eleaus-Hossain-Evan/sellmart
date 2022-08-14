ac3d8116e87c5bf5e0a1b71d9faec36f531e4fb4
Eleaus-Hossain-Evan <evanhossain62@gmail.com>
2022-08-14 12:33:05 +0600

    remove my_order from account tab

--- lib/widget/menu_widget.dart
+++ lib/widget/menu_widget.dart
@@ -12,90 +12,89 @@ import '../view/bottom_nav.dart';
 import '../view/web_view.dart';
 
 class MenuWidget extends StatefulWidget {
-
   @override
   _MenuWidgetState createState() => _MenuWidgetState();
 }
 
 class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
-
   DBHelper _dbHelper = DBHelper();
   MySharedPreference _sharedPreference = MySharedPreference();
 
   @override
   Widget build(BuildContext context) {
-
     return ValueListenableBuilder(
       valueListenable: currentUser,
       builder: (BuildContext context, User user, _) {
-
         return Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-
-            SizedBox(height: 6.5 * SizeConfig.heightSizeMultiplier,),
+            SizedBox(
+              height: 6.5 * SizeConfig.heightSizeMultiplier,
+            ),
 
             Visibility(
               visible: user != null && user.id != null && user.id.isNotEmpty,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.PROFILE_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.profile,
+                leading: Image.asset(
+                  Images.profile,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("profile"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("profile"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
+
+            // Visibility(
+            //   visible: user != null && user.id != null && user.id.isNotEmpty,
+            //   child: ListTile(
+            //     onTap: () {
+
+            //       _onItemClick(Constants.MY_ORDER_MENU);
+            //     },
+            //     dense: true,
+            //     leading: Image.asset(Images.shoppingBag,
+            //       height: 3.125 * SizeConfig.heightSizeMultiplier,
+            //       width: 6.41 * SizeConfig.widthSizeMultiplier,
+            //       fit: BoxFit.fill,
+            //     ),
+            //     title: Text(AppLocalization.of(context).getTranslatedValue("my_orders"),
+            //       style: Theme.of(context).textTheme.subtitle2.copyWith(
+            //         fontWeight: FontWeight.w400,
+            //       ),
+            //     ),
+            //   ),
+            // ),
 
             Visibility(
               visible: user != null && user.id != null && user.id.isNotEmpty,
               child: ListTile(
                 onTap: () {
-
-                  _onItemClick(Constants.MY_ORDER_MENU);
-                },
-                dense: true,
-                leading: Image.asset(Images.shoppingBag,
-                  height: 3.125 * SizeConfig.heightSizeMultiplier,
-                  width: 6.41 * SizeConfig.widthSizeMultiplier,
-                  fit: BoxFit.fill,
-                ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("my_orders"),
-                  style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-
-            Visibility(
-              visible: user != null && user.id != null && user.id.isNotEmpty,
-              child: ListTile(
-                onTap: () {
-
                   _onItemClick(Constants.WISH_LIST_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.wishList,
+                leading: Image.asset(
+                  Images.wishList,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("wish_list"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("wish_list"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -104,19 +103,20 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: user != null && user.id != null && user.id.isNotEmpty,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.ADDRESS_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.address,
+                leading: Image.asset(
+                  Images.address,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("address"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("address"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -125,19 +125,21 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: user != null && user.id != null && user.id.isNotEmpty,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.CHANGE_PASSWORD_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.changePassword,
+                leading: Image.asset(
+                  Images.changePassword,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("change_password"),
+                title: Text(
+                  AppLocalization.of(context)
+                      .getTranslatedValue("change_password"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -146,74 +148,81 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: user != null && user.id != null && user.id.isNotEmpty,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.REFUND_SETTLEMENT_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.refund,
+                leading: Image.asset(
+                  Images.refund,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("return_refund"),
+                title: Text(
+                  AppLocalization.of(context)
+                      .getTranslatedValue("return_refund"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
 
             ListTile(
               onTap: () {
-
                 _onItemClick(Constants.TERMS_CONDITIONS_MENU);
               },
               dense: true,
-              leading: Image.asset(Images.terms,
+              leading: Image.asset(
+                Images.terms,
                 height: 3.125 * SizeConfig.heightSizeMultiplier,
                 width: 6.41 * SizeConfig.widthSizeMultiplier,
                 fit: BoxFit.fill,
               ),
-              title: Text(AppLocalization.of(context).getTranslatedValue("terms_condition"),
+              title: Text(
+                AppLocalization.of(context)
+                    .getTranslatedValue("terms_condition"),
                 style: Theme.of(context).textTheme.subtitle2.copyWith(
-                  fontWeight: FontWeight.w400,
-                ),
+                      fontWeight: FontWeight.w400,
+                    ),
               ),
             ),
 
             ListTile(
               onTap: () {
-
                 _onItemClick(Constants.PRIVACY_POLICY_MENU);
               },
               dense: true,
-              leading: Image.asset(Images.privacy,
+              leading: Image.asset(
+                Images.privacy,
                 height: 3.125 * SizeConfig.heightSizeMultiplier,
                 width: 6.41 * SizeConfig.widthSizeMultiplier,
                 fit: BoxFit.fill,
               ),
-              title: Text(AppLocalization.of(context).getTranslatedValue("privacy_policy"),
+              title: Text(
+                AppLocalization.of(context)
+                    .getTranslatedValue("privacy_policy"),
                 style: Theme.of(context).textTheme.subtitle2.copyWith(
-                  fontWeight: FontWeight.w400,
-                ),
+                      fontWeight: FontWeight.w400,
+                    ),
               ),
             ),
 
             ListTile(
               onTap: () {
-
                 _onItemClick(Constants.REPORT_ISSUE_MENU);
               },
               dense: true,
-              leading: Image.asset(Images.report,
+              leading: Image.asset(
+                Images.report,
                 height: 3.125 * SizeConfig.heightSizeMultiplier,
                 width: 6.41 * SizeConfig.widthSizeMultiplier,
                 fit: BoxFit.fill,
               ),
-              title: Text(AppLocalization.of(context).getTranslatedValue("report_issue"),
+              title: Text(
+                AppLocalization.of(context).getTranslatedValue("report_issue"),
                 style: Theme.of(context).textTheme.subtitle2.copyWith(
-                  fontWeight: FontWeight.w400,
-                ),
+                      fontWeight: FontWeight.w400,
+                    ),
               ),
             ),
 
@@ -221,19 +230,20 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: false,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.ABOUT_US_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.information,
+                leading: Image.asset(
+                  Images.information,
                   height: 3.75 * SizeConfig.heightSizeMultiplier,
                   width: 7.69 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("about_us"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("about_us"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -242,19 +252,20 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: false,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.LANGUAGE_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.translate,
+                leading: Image.asset(
+                  Images.translate,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("language"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("language"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -263,19 +274,20 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: user != null && user.id != null && user.id.isNotEmpty,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.LOGOUT_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.logout,
+                leading: Image.asset(
+                  Images.logout,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("logout"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("logout"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -284,19 +296,20 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
               visible: user == null || user.id == null || user.id.isEmpty,
               child: ListTile(
                 onTap: () {
-
                   _onItemClick(Constants.LOGIN_MENU);
                 },
                 dense: true,
-                leading: Image.asset(Images.enter,
+                leading: Image.asset(
+                  Images.enter,
                   height: 3.125 * SizeConfig.heightSizeMultiplier,
                   width: 6.41 * SizeConfig.widthSizeMultiplier,
                   fit: BoxFit.fill,
                 ),
-                title: Text(AppLocalization.of(context).getTranslatedValue("login"),
+                title: Text(
+                  AppLocalization.of(context).getTranslatedValue("login"),
                   style: Theme.of(context).textTheme.subtitle2.copyWith(
-                    fontWeight: FontWeight.w400,
-                  ),
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
             ),
@@ -306,11 +319,8 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
     );
   }
 
-
   void _onItemClick(int value) {
-
-    switch(value) {
-
+    switch (value) {
       case Constants.PROFILE_MENU:
         _openMyProfilePage();
         break;
@@ -332,15 +342,21 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
         break;
 
       case Constants.REFUND_SETTLEMENT_MENU:
-        _openWebViewPage(AppLocalization.of(context).getTranslatedValue("return_refund"), Constants.RETURN_POLICY);
+        _openWebViewPage(
+            AppLocalization.of(context).getTranslatedValue("return_refund"),
+            Constants.RETURN_POLICY);
         break;
 
       case Constants.TERMS_CONDITIONS_MENU:
-        _openWebViewPage(AppLocalization.of(context).getTranslatedValue("terms_condition"), Constants.TERMS_CONDITIONS);
+        _openWebViewPage(
+            AppLocalization.of(context).getTranslatedValue("terms_condition"),
+            Constants.TERMS_CONDITIONS);
         break;
 
       case Constants.PRIVACY_POLICY_MENU:
-        _openWebViewPage(AppLocalization.of(context).getTranslatedValue("privacy_policy"), Constants.PRIVACY_POLICY);
+        _openWebViewPage(
+            AppLocalization.of(context).getTranslatedValue("privacy_policy"),
+            Constants.PRIVACY_POLICY);
         break;
 
       case Constants.REPORT_ISSUE_MENU:
@@ -364,62 +380,44 @@ class _MenuWidgetState extends State<MenuWidget> with ChangeNotifier {
     }
   }
 
-
   Future<void> _openMyProfilePage() async {
-
     Navigator.of(context).pushNamed(RouteManager.MY_PROFILE);
   }
 
-
   Future<void> _openMyOrdersPage() async {
-
     Navigator.of(context).pushNamed(RouteManager.MY_ORDERS);
   }
 
   Future<void> _openMyWishListPage() async {
-
     Navigator.of(context).pushNamed(RouteManager.WISH_LIST);
   }
 
-
   Future<void> _openWebViewPage(String title, int type) async {
-
-    Navigator.push(context, MaterialPageRoute(builder: (context) => WebView(title, type)));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => WebView(title, type)));
   }
 
-
   Future<void> _openAddressPage() async {
-
     Navigator.of(context).pushNamed(RouteManager.MY_ADDRESS);
   }
 
-
   Future<void> _openChangePasswordPage() async {
-
     Navigator.of(context).pushNamed(RouteManager.CHANGE_PASSWORD);
   }
 
-
   Future<void> _openAboutUsPage() async {
-
     Navigator.of(context).pushNamed(RouteManager.ABOUT_US);
   }
 
-
   Future<void> _openLanguagePage() async {
-
     Navigator.of(context).pushNamed(RouteManager.LANGUAGE);
   }
 
-
   Future<void> _login() async {
-
     Navigator.of(context).pushNamed(RouteManager.LOGIN);
   }
 
-
   Future<void> _logOut() async {
-
     await _dbHelper.deleteAllProduct();
 
     numberOfItems.value = await _dbHelper.getProductCount();
